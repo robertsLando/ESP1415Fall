728a38489a0b1cbaa08b398a4d7e2155deddda83
daniellando <daniellando@192.168.1.8>
2015-05-02 20:49:03 +0200

    Fab button hiding when scroll to end

--- FallDetector/src/unipd/dei/ESP1415/falldetector/MainActivity.java
+++ FallDetector/src/unipd/dei/ESP1415/falldetector/MainActivity.java
@@ -10,47 +10,75 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-	ListView list; //the reference to the widget in main activity
-	sessionViewAdapter adapter; //the adapter for listview manage
-	public ArrayList<Session> listViewValues = new ArrayList<Session>(); //the container
+	ListView list; // the reference to the widget in main activity
+	sessionViewAdapter adapter; // the adapter for listview manage
+	public ArrayList<Session> listViewValues = new ArrayList<Session>(); // the
+																			// container
 	private static FloatingActionButton fabButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		// draw the fab button
 		fabButton = fabSetter();
-		
-		
-		//add the elements into the listview
+
+		// add the elements into the listview
 		setListData();
-		
-		//get the listview widget
+
+		// get the listview widget
 		list = (ListView) findViewById(R.id.sessionListView);
-		
-		
-		//create a new adapter for the listview
+
+		// create a new adapter for the listview
 		adapter = new sessionViewAdapter(this, listViewValues);
 		list.setAdapter(adapter);
+		
+		//to hide action button when scrolling
+		list.setOnScrollListener(new OnScrollListener() {
+
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				if (firstVisibleItem + visibleItemCount == totalItemCount
+						&& totalItemCount != 0) {
+					fabButton.hideFloatingActionButton();
+					fabButton.setVisibility(View.GONE);
+				}
+
+				else
+				{
+					fabButton.showFloatingActionButton();
+					fabButton.setVisibility(View.VISIBLE);
+				}
+
+			}
+		});
 
 	}
 
 	public void setListData() {
 
-		//TODO: Add database support fetching
+		// TODO: Add database support fetching
 		for (int i = 0; i < 11; i++) {
 
 			final Session temp = new Session();
 
 			temp.setName("NomeSessioneeeeeeeeee");
-			temp.setEnd(0); //oggi
-			temp.setStart(1430489157); //domani
+			temp.setEnd(0); // oggi
+			temp.setStart(1430489157); // domani
 			temp.setFalls(1000);
 
 			listViewValues.add(temp);
@@ -58,42 +86,43 @@ public class MainActivity extends ActionBarActivity {
 
 	}
 
-	public void onItemClick(int mPosition)
-    {
-        Session temp = (Session) listViewValues.get(mPosition);                  
-        
-        //Show the alert
-        Toast.makeText(this,"Hai premuto l'elemento: " + temp.getName(),
-                Toast.LENGTH_LONG)
-        .show();
-        
-    }
-	
-	private FloatingActionButton fabSetter(){
-		// Set ad final otherwise there are problem on the Listener 
+	public void onItemClick(int mPosition) {
+		Session temp = (Session) listViewValues.get(mPosition);
+
+		// Show the alert
+		Toast.makeText(this, "Hai premuto l'elemento: " + temp.getName(),
+				Toast.LENGTH_LONG).show();
+
+	}
+
+	private FloatingActionButton fabSetter() {
+		// Set ad final otherwise there are problem on the Listener
 		final FloatingActionButton fabButton;
 
-		fabButton= new FloatingActionButton.Builder(this)
-		.withDrawable(getResources().getDrawable(R.drawable.ic_plus_black_24dp))
-		.withButtonColor(Color.WHITE)
-		.withGravity(Gravity.BOTTOM | Gravity.END)
-		.withMargins(0, 0, 16, 16).create();
+		fabButton = new FloatingActionButton.Builder(this)
+				.withDrawable(
+						getResources().getDrawable(
+								R.drawable.ic_plus_black_24dp))
+				.withButtonColor(Color.WHITE)
+				.withGravity(Gravity.BOTTOM | Gravity.END)
+				.withMargins(0, 0, 16, 16).create();
 
-		fabButton.setOnClickListener(new View.OnClickListener(){
+		fabButton.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v){
-				if(!fabButton.isHidden()){
-					Toast.makeText(v.getContext(), "Hai premuto il FAB", Toast.LENGTH_SHORT).show();
-				}//if
-			}//onCLick
-		});//setOnClickListener
+			public void onClick(View v) {
+				if (!fabButton.isHidden()) {
+					Toast.makeText(v.getContext(), "Hai premuto il FAB",
+							Toast.LENGTH_SHORT).show();
+				}// if
+			}// onCLick
+		});// setOnClickListener
 
 		return fabButton;
-	}//fabSetter
-	
-	public static FloatingActionButton getFAB(){
+	}// fabSetter
+
+	public static FloatingActionButton getFAB() {
 		return fabButton;
-	}//getFAB
+	}// getFAB
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
