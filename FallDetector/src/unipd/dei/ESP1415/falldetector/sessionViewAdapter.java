dbd1706e0ddedef064226367f85cf458db22e3bb
daniellando <daniellando@192.168.1.8>
2015-05-02 12:57:26 +0200

    random icon and more button menu added

--- FallDetector/src/unipd/dei/ESP1415/falldetector/sessionViewAdapter.java
+++ FallDetector/src/unipd/dei/ESP1415/falldetector/sessionViewAdapter.java
@@ -3,11 +3,16 @@ package unipd.dei.ESP1415.falldetector;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Random;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.PopupMenu;
+import android.test.MoreAsserts;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -15,17 +20,20 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * The adapter for the list view
+ * 
  * @author daniellando
  *
  */
 public class sessionViewAdapter extends BaseAdapter implements OnClickListener {
 
-	private Activity activity; //the activity where the ListView is placed
-	private ArrayList<Session> sessionList; //the container
-	private static LayoutInflater inflater = null; // calls external xml layout ()
+	private Activity activity; // the activity where the ListView is placed
+	private ArrayList<Session> sessionList; // the container
+	private static LayoutInflater inflater = null; // calls external xml layout
+													// ()
 	private SessionViewHolder itemVisible = null;
 
 	Session ses = null;
@@ -68,32 +76,42 @@ public class sessionViewAdapter extends BaseAdapter implements OnClickListener {
 
 		if (mySessionView == null) {
 
-			//set the layout of the item
+			// set the layout of the item
 			mySessionView = inflater.inflate(R.layout.session_child, null);
 
-			//create the item holder
+			// create the item holder
 			holder = new SessionViewHolder();
 			// Get widgets from the view
 			holder.sessionName = (TextView) mySessionView
 					.findViewById(R.id.sessionName);
-			holder.falls = (TextView) mySessionView.findViewById(R.id.fallsNumber);
+			holder.falls = (TextView) mySessionView
+					.findViewById(R.id.fallsNumber);
 			holder.startTime = (TextView) mySessionView
 					.findViewById(R.id.startTime);
-			holder.endTime = (TextView) mySessionView.findViewById(R.id.endTime);
+			holder.endTime = (TextView) mySessionView
+					.findViewById(R.id.endTime);
 			holder.durationTime = (TextView) mySessionView
 					.findViewById(R.id.durationTime);
-			
-			holder.expandable = (RelativeLayout) mySessionView.findViewById(R.id.expandableLayout);
-			holder.playButton = (ImageView) mySessionView.findViewById(R.id.playButton);
-			holder.pauseButton = (ImageView) mySessionView.findViewById(R.id.pauseButton);
+
+			holder.expandable = (RelativeLayout) mySessionView
+					.findViewById(R.id.expandableLayout);
+			holder.playButton = (ImageView) mySessionView
+					.findViewById(R.id.playButton);
+			holder.pauseButton = (ImageView) mySessionView
+					.findViewById(R.id.pauseButton);
+			holder.fallIcon = (ImageView) mySessionView
+					.findViewById(R.id.sessionImage);
+			holder.moreButton = (ImageView) mySessionView
+					.findViewById(R.id.moreButton);
 
 			mySessionView.setTag(holder);
-			
-		} else //the view already exist
+
+		} else
+			// the view already exist
 			holder = (SessionViewHolder) mySessionView.getTag();
 
 		ses = null;
-		ses = (Session)sessionList.get(position);
+		ses = (Session) sessionList.get(position);
 
 		Date start = new Date(ses.getStart());
 		Date end = new Date(ses.getEnd());
@@ -112,28 +130,57 @@ public class sessionViewAdapter extends BaseAdapter implements OnClickListener {
 		holder.startTime.setText(getDate(start));
 		holder.endTime.setText(getDate(end));
 		holder.durationTime.setText(duration);
-		
-		if(ses.getEnd() == 0) //E' in esecuzione
+
+		// generate the random bgcolor
+		Random rnd = new Random();
+		int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256),
+				rnd.nextInt(256));
+		holder.fallIcon.setBackgroundColor(color);
+
+		if (ses.getEnd() == 0) // E' in esecuzione
 		{
 			holder.playButton.setVisibility(View.VISIBLE);
 		}
-			
-		
+
+		holder.moreButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(final View v) {
+				// Creating the instance of PopupMenu
+				PopupMenu popup = new PopupMenu(v.getContext(), holder.moreButton);
+				// Inflating the Popup using xml file
+				popup.getMenuInflater().inflate(R.menu.more_menu,
+						popup.getMenu());
+
+				// registering popup with OnMenuItemClickListener
+				popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+					public boolean onMenuItemClick(MenuItem item) {
+						Toast.makeText(v.getContext(),
+								"You Clicked : " + item.getTitle(),
+								Toast.LENGTH_SHORT).show();
+						return true;
+					}
+				});
+
+				popup.show(); // showing popup menu
+			}
+		}); // closing the setOnClickListener method
+
 		holder.playButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				holder.pauseButton.setVisibility(View.VISIBLE);
-				
+
 			}
 		});
-		
+
 		holder.pauseButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				holder.playButton.setVisibility(View.VISIBLE);
-				
+
 			}
 		});
 
@@ -154,23 +201,18 @@ public class sessionViewAdapter extends BaseAdapter implements OnClickListener {
 		public void onClick(View myView) {
 
 			SessionViewHolder holder = (SessionViewHolder) myView.getTag();
-			if(itemVisible == null)
-			{
+			if (itemVisible == null) {
 				holder.expandable.setVisibility(View.VISIBLE);
 				itemVisible = holder;
-			}
-			else if(itemVisible == holder)
-			{
+			} else if (itemVisible == holder) {
 				holder.expandable.setVisibility(View.GONE);
 				itemVisible = null;
-			}
-			else
-			{
+			} else {
 				holder.expandable.setVisibility(View.VISIBLE);
 				itemVisible.expandable.setVisibility(View.GONE);
 				itemVisible = holder;
 			}
-			
+
 			MainActivity sct = (MainActivity) activity;
 			sct.onItemClick(mPosition);
 		}
@@ -178,7 +220,9 @@ public class sessionViewAdapter extends BaseAdapter implements OnClickListener {
 
 	/**
 	 * A method to get a date in the format dd-MM-yyyy HH:mm:ss
-	 * @param date the date to format
+	 * 
+	 * @param date
+	 *            the date to format
 	 * @return the date formatted
 	 */
 	private String getDate(Date date) {
@@ -188,13 +232,15 @@ public class sessionViewAdapter extends BaseAdapter implements OnClickListener {
 					"dd-MM-yyyy HH:mm:ss");
 			return format.format(date);
 		} catch (Exception e) {
-			Log.e("Date format EXCEPTION", "Something went wrong with date formatting");
+			Log.e("Date format EXCEPTION",
+					"Something went wrong with date formatting");
 			return null;
 		}
 	}
 
 	/**
-	 * The holder for each ListView  Session element
+	 * The holder for each ListView Session element
+	 * 
 	 * @author daniellando
 	 *
 	 */
@@ -208,6 +254,8 @@ public class sessionViewAdapter extends BaseAdapter implements OnClickListener {
 		public RelativeLayout expandable;
 		public ImageView playButton;
 		public ImageView pauseButton;
+		public ImageView moreButton;
+		public ImageView fallIcon;
 
 	}
 
